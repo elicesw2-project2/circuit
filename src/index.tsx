85819dc6480466732df480d66fc26b8afa60b8fe
ctd <qpflapffhs76@gmail.com>
2022-04-26 01:36:09 +0900

    ESlint + Prettier 환경 설정

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
