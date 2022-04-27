61d76180ec57d7af5ccab73477541235e4402a1c
ctd <qpflapffhs76@gmail.com>
2022-04-28 02:01:39 +0900

    코딩 컨벤션 세팅

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = import('express');
+
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+	res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
