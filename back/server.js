8e228abf33e7bf9bac30b438be8a4a784778495b
ctd <qpflapffhs76@gmail.com>
2022-05-03 16:52:16 +0900

    배포용 포트 번호 수정

--- back/server.js
+++ back/server.js
@@ -3,6 +3,8 @@ const bodyParser = import('body-parser');
 
 const app = express();
 
+const PORT = process.env.PORT || 3001;
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -11,6 +13,6 @@ app.get('/', (req, res) => {
 });
 
 // 포트넘버 설정
-app.listen(3001, () => {
+app.listen(PORT, () => {
 	console.log('Server is running on port 3001.');
 });
