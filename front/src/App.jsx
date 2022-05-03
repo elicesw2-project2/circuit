c61e29e5a5faa28a824a2d5d3971db76ce88ef1d
ctd <qpflapffhs76@gmail.com>
2022-05-03 23:14:32 +0900

    yarn.lock

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
 import './App.css';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
