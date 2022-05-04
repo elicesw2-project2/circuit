3feca0b7d5150365d38fcb41bc6ecbf6d50ed385
ctd <qpflapffhs76@gmail.com>
2022-05-04 13:21:36 +0900

    BrowserRouter basename update

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
@@ -8,13 +8,11 @@ import Story from './components/Story';
 function App() {
 	return (
 		<div className="App">
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<Login />} />
-					<Route path="/signUp" element={<SignUp />} />
-					<Route path="/Story" element={<Story />} />
-				</Routes>
-			</BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Login />} />
+				<Route path="/signUp" element={<SignUp />} />
+				<Route path="/Story" element={<Story />} />
+			</Routes>
 		</div>
 	);
 }
