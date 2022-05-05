cceecc19a2f07f6194570bac6f9a172b77a12761
ctd <qpflapffhs76@gmail.com>
2022-05-05 18:33:13 +0900

    개발 전 style reset

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import MyPage from 'pages/MyPage';
+import MainPage from 'pages/MainPage';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import Story from './components/Story';
@@ -9,8 +11,10 @@ function App() {
 	return (
 		<div className="App">
 			<Routes>
-				<Route path="/" element={<Login />} />
+				<Route path="/" element={<MainPage />} />
+				<Route path="/login" element={<Login />} />
 				<Route path="/signUp" element={<SignUp />} />
+				<Route path="my-page" element={<MyPage />} />
 				<Route path="/Story" element={<Story />} />
 			</Routes>
 		</div>
--- front/src/pages/MainPage.jsx
+++ front/src/pages/MainPage.jsx
@@ -7,7 +7,6 @@ function MainPage() {
 		<>
 			<NavBar />
 			<Profile />
-			<div />
 		</>
 	);
 }
