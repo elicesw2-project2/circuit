c47b437d1dc6ef4a73b8c0ff52e55d8619f3e2c9
Hye-Ji Na <nhj7911@gmail.com>
2022-05-08 17:59:11 +0900

    jawsDB mysql 연결

--- back/config/db.config.js
+++ back/config/db.config.js
@@ -1,8 +1,8 @@
 const dbConfig = {
-	HOST: 'localhost',
-	USER: 'elice',
-	PASSWORD: '1111',
-	DB: 'elice_db',
+	HOST: 'dt3bgg3gu6nqye5f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+	USER: '	t0dn6h8s29ti3206',
+	PASSWORD: 'jnl72bdbmthtu24l',
+	DB: 'giw3qxk1yvxgfe3k',
 };
 
 export default dbConfig;
