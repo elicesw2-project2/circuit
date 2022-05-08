57272519d9720a43562b965948e6c65768f34d38
ctd <qpflapffhs76@gmail.com>
2022-05-08 18:23:17 +0900

    db config 수정

--- back/models/db.js
+++ back/models/db.js
@@ -5,6 +5,7 @@ import dbConfig from '../config/db.config.js';
 const connection = mysql.createConnection({
 	host: dbConfig.HOST,
 	user: dbConfig.USER,
+	port: dbConfig.PORT,
 	password: dbConfig.PASSWORD,
 	database: dbConfig.DB,
 });
