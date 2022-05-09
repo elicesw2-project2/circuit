9bb2cc20a123a937e6a4382a36816e4de2d5ea27
ctd <qpflapffhs76@gmail.com>
2022-05-09 23:58:00 +0900

    메인 프로필 수정

--- front/src/components/MainProfile.jsx
+++ front/src/components/MainProfile.jsx
@@ -7,13 +7,17 @@ import { Link } from 'react-router-dom';
 function MainProfile() {
 	return (
 		<div className="MainProfile">
-			<img src={profile} alt="profile" />
-			<button type="button">
+			<Link to="/my-page">
+				<img src={profile} alt="profile" />
+			</Link>
+			<h2>엘리스</h2>
+			<span>elice123@gmail.com</span>
+			{/* <button type="button">
 				<Link to="/my-page">마이페이지</Link>
 			</button>
 			<button type="button" onClick={Logout}>
 				<Link to="/login">로그아웃</Link>
-			</button>
+			</button> */}
 		</div>
 	);
 }
