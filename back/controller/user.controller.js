f521da2813fd09bf12a973046909f79b7227e891
siwon <72402014+cone26@users.noreply.github.com>
2022-05-10 19:44:49 +0900

    login debugging (mysql 연결 문제)

--- back/controller/user.controller.js
+++ back/controller/user.controller.js
@@ -1,83 +1,32 @@
 import jwt from 'jsonwebtoken';
-import User from '../models/auth.model.js';
+// import User from '../models/auth.model.js';
+import sql from '../models/db.js';
 
 const AUTH_ERROR = { message: 'Authentication Error' };
 
 // 새 객체 생성
-export function create(req, res) {
-	if (!req.body) {
-		res.status(400).send({
-			message: 'Content can not be empty!',
-		});
-	}
-
-	const User = new User({
-		email: req.body.email,
-		password: req.body.password,
-		nickname: req.body.nickname,
-		url: req.body.url,
-	});
-
-	// 데이터베이스에 저장
-	User.createUser(User, (err, data) => {
-		if (err) {
-			res.status(500).send({
-				message: err.message || 'Some error occured while creating th Customer.',
-			});
-		}
+export async function createUser(user) {
+	const { id, pw, nickname, profile } = user;
+	sql.query('INSERT INTO user(id, pw, nickname, profile) VALUES (?,?,?,?)', [id, pw, nickname, profile], (results) => {
+		console.log(results);
+		return results;
 	});
 }
 
 // id로 조회
-export function findByUserid(req, res) {
-	User.findById(req.params.userId, (err, data) => {
-		if (err) {
-			if (err.kind === 'not_found') {
-				res.status(404).send({
-					message: `Not found Customer with id ${req.params.userId}.`,
-				});
-			} else {
-				res.status(500).send({
-					message: `Error retrieving Customer with id ${req.params.userId}`,
-				});
-			}
-		} else res.send(data);
+export function findByUserid(id) {
+	// sql.query('SELECT * FROM user WHERE id =?', id, (err, result) => {
+	// 	result.redirect(JSON.stringify(result));
+	// });
+	// sql.query('SELECT * FROM user WHERE id =?', id, (err, result) => {
+	// 	if (err) {
+	// 		callback(err, null);
+	// 	}
+	// 	return callback(null, result[0]);
+	// });
+	return new Promise((resolve, reject) => {
+		sql.query(`SELECT * FROM giw3qxk1yvxgfe3k.user WHERE id='${id}'`, (err, result) => {
+			return err ? reject(err) : resolve(result[0]);
+		});
 	});
 }
-
-// email로 조회
-export function findOneByEmail(req, res) {
-	User.findByEmail(req.params.userEmail, (err, data) => {
-		if (err) {
-			if (err.kind === 'not_found') {
-				res.status(404).send({
-					message: `Not found Customer with id ${req.params.userEmail}.`,
-				});
-			} else {
-				res.status(500).send({
-					message: `Error retrieving Customer with id ${req.params.userEmail}`,
-				});
-			}
-		} else res.send(data);
-	});
-}
-
-export const isAuth = async (req, res, next) => {
-	const authHeader = req.get('Authorization');
-	if (!(authHeader && authHeader.startsWith('Bearer '))) {
-		return res.status(401).json(AUTH_ERROR);
-	}
-	const token = authHeader.aplit(' ')[1];
-
-	jwt.verify(token, 'PV59zFWNbrvJrfDLkETWe2VHZttSfSq9', async (error, decoded) => {
-		if (error) {
-			return res.status(401).json(AUTH_ERROR);
-		}
-		const user = await findByUserid(decoded.id);
-		if (!user) {
-			return res.status(401).json(AUTH_ERROR);
-		}
-		req.userId = user.id;
-		next();
-	});
-};
