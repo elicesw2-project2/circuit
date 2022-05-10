e7845170ce4de2ecd9fa86f0adf76f804bc1390c
siwon <72402014+cone26@users.noreply.github.com>
2022-05-10 20:15:18 +0900

    B로그인-코드 디테일 수정

--- back/controller/user.controller.js
+++ back/controller/user.controller.js
@@ -1,8 +1,4 @@
-import jwt from 'jsonwebtoken';
-// import User from '../models/auth.model.js';
 import sql from '../models/db.js';
-
-const AUTH_ERROR = { message: 'Authentication Error' };
 
 // 새 객체 생성
 export async function createUser(user) {
@@ -15,15 +11,6 @@ export async function createUser(user) {
 
 // id로 조회
 export function findByUserid(id) {
-	// sql.query('SELECT * FROM user WHERE id =?', id, (err, result) => {
-	// 	result.redirect(JSON.stringify(result));
-	// });
-	// sql.query('SELECT * FROM user WHERE id =?', id, (err, result) => {
-	// 	if (err) {
-	// 		callback(err, null);
-	// 	}
-	// 	return callback(null, result[0]);
-	// });
 	return new Promise((resolve, reject) => {
 		sql.query(`SELECT * FROM giw3qxk1yvxgfe3k.user WHERE id='${id}'`, (err, result) => {
 			return err ? reject(err) : resolve(result[0]);
