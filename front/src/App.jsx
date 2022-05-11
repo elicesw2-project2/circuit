17351cf4c17a809b8839efd9fde662a63ed31772
[SW2/서울/2반]성경주 <yesican1011@naver.com>
2022-05-11 21:19:40 +0900

    글 작성 페이지, 메인페이지 레이아웃 수정

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -7,20 +7,20 @@ import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import Story from './components/Story';
 import Read from './pages/Read';
+import Writing from './pages/Writing';
 import MainPage from './pages/MainPage';
 
 function App() {
 	return (
 		<div className="App">
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<Login />} />
-					<Route path="/signUp" element={<SignUp />} />
-					<Route path="/Story" element={<Story />} />
-					<Route path="/Read/:id" element={<Read />} />
-					<Route path="/MainPage" element={<MainPage />} />
-				</Routes>
-			</BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Login />} />
+				<Route path="/signUp" element={<SignUp />} />
+				<Route path="/Story" element={<Story />} />
+				<Route path="/Read/:id" element={<Read />} />
+				<Route path="/Writing" element={<Writing />} />
+				<Route path="/MainPage" element={<MainPage />} />
+			</Routes>
 		</div>
 	);
 }
--- front/src/pages/MainPage.jsx
+++ front/src/pages/MainPage.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import NavBar from 'components/NavBar';
 import MainProfile from 'components/MainProfile';
 import Story from 'components/Story';
+import '../styles/MainPage.scss';
 
 function MainPage() {
 	return (
 		<>
 			<NavBar />
-			<MainProfile />
-
-			<div className="test">
+			<div className="main_cpn">
 				<Story />
+				<MainProfile />
 			</div>
 
 			<div />
