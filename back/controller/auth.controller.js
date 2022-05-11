6f03e39e93b5ca476050f1c404ba364416dcdbc0
siwon <72402014+cone26@users.noreply.github.com>
2022-05-11 14:21:13 +0900

    login res 디테일 수정

--- back/controller/auth.controller.js
+++ back/controller/auth.controller.js
@@ -14,7 +14,7 @@ export async function signup(req, res) {
 
 	console.log(`found:${found}`);
 	if (found) {
-		return res.status(409).json({ mesage: `${id} already exists` });
+		return res.status(409).json({ status: 409, message: `${id} already exists` });
 	}
 	const hashed = await bcrypt.hash(pw, bcryptSaltRounds);
 	const userId = await userRepository.createUser({
@@ -23,8 +23,8 @@ export async function signup(req, res) {
 		nickname,
 		profile,
 	});
-	const token = createJwtToken(id);
-	res.status(201).json({ token, id });
+	const token = createJwtToken(userId);
+	res.status(201).json({ status: 201, message: '회원가입 성공', data: { token, id } });
 }
 
 // login 확인 + 토큰
@@ -32,20 +32,21 @@ export async function login(req, res) {
 	const { id, pw } = req.body;
 	const user = await userRepository.findByUserid(id);
 	if (!user) {
-		return res.status(404).json({ message: 'User not found' });
+		return res.status(404).json({ status: 404, message: 'User not found' });
 	}
+
 	const isValidPassword = await bcrypt.compare(pw, user.pw);
 	console.log('isValidPassword', isValidPassword);
 	if (!isValidPassword) {
-		return res.status(401).json({ mesage: `Invalid email or password` });
+		return res.status(401).json({ status: 401, message: `Invalid email or password` });
 	}
 	if (user.id == id && isValidPassword) {
 		const token = createJwtToken(id);
 		console.log('성공');
-		res.status(200).json({ token, id });
+		res.status(200).json({ status: 200, message: '로그인 성공', data: { token, id } });
 	} else {
 		console.log('실패');
-		return res.status(401).json({ mesage: `Invalid email or password` });
+		return res.status(401).json({ status: 401, mesage: `Invalid email or password` });
 	}
 }
 
@@ -57,7 +58,8 @@ function createJwtToken(id) {
 export async function me(req, res, next) {
 	const user = await userRepository.findByUserid(req.headers.id);
 	if (!user) {
-		return res.status(404).json({ message: 'User not found' });
+		return res.status(404).json({ status: 404, message: 'User not found' });
 	}
-	return res.status(200).json({ token: req.token, id: req.headers.id });
+	console.log(req.headers);
+	return res.status(200).json({ status: 200, message: '토큰 일치', data: { id: req.headers.id } });
 }
