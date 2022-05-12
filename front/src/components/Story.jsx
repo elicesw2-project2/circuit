ddd90e986a2c4040e2dc47e6e877328c946fce7f
[SW2/서울/2반]성경주 <yesican1011@naver.com>
2022-05-12 17:15:03 +0900

    get 기능 테스트

--- front/src/components/Story.jsx
+++ front/src/components/Story.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Story.scss';
 import dummy from '../db/story.json';
@@ -8,39 +8,58 @@ import dummy from '../db/story.json';
 // })
 // 	.then((res) => res.json())
 // 	.then((data) => {
-// 		console.log(data.data);
+// 		console.log(data);
 // 		// data.data.map((el) => console.log(el.post_idx));
+// 		data.data.map((el) => (
+// 			<div className="story">
+// 				<span className="story_name story_child asdf">{el.nickname}</span>
+// 				<Link to={`/Read/${el.id}`}>
+// 					<span className="story_title story_child">{el.title}</span>
+// 				</Link>
+// 				<span className="story_time story_child">{el.date}</span>
+// 			</div>
+// 		));
 // 	});
 
 function Story() {
 	// 글 목록 컨테이너 , 이 안에 글 들이 들어감
-	// <Link to={`/Read/${el.id}`}> 더미데이터의 id 값을 map을 이용해 주소로 만들어 목록 생성
 	return (
 		<section className="story_container">
 			<StoryInfo />
-			{dummy.info.map((el) => (
-				<div className="story">
-					<span className="story_name story_child">{el.name}</span>
-					<Link to={`/Read/${el.id}`}>
-						<span className="story_title story_child">{el.title}</span>
-					</Link>
-					<span className="story_time story_child">{el.day}</span>
-				</div>
-			))}
+			<Storys />
 		</section>
 	);
 }
 
-// function Storys() {
-// 	// 글 목록의 리스트 들 (형태를 잡기위해 만들어 둔것으로 이젠 삭제해도됨)
-// 	return (
-// 		<div className="story">
-// 			<span className="story_name story_child">작성자</span>
-// 			<span className="story_title story_child">제목이 엄청 길어</span>
-// 			<span className="story_time story_child">2022.05.03</span>
-// 		</div>
-// 	);
-// }
+function Storys() {
+	// <Link to={`/Read/${el.id}`}> 더미데이터의 id 값을 map을 이용해 주소로 만들어 목록 생성
+	const [Test, setTest] = useState(null);
+	const [Testtt, setTesttt] = useState(null);
+
+	useEffect(() => {
+		fetch('https://elice-server.herokuapp.com/board', {
+			method: 'GET',
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data.data);
+				setTest(data.data);
+				setTesttt(data.data[0]);
+			});
+	}, []);
+	console.log(Test);
+	// console.log(Test[0]);
+
+	return dummy.info.map((el) => (
+		<div className="story">
+			<span className="story_name story_child asdf">{el.nickname}</span>
+			<Link to={`/Read/${el.id}`}>
+				<span className="story_title story_child">{el.title}</span>
+			</Link>
+			<span className="story_time story_child">{el.date}</span>
+		</div>
+	));
+}
 
 function StoryInfo() {
 	// 글 목록의 구성( 작성자, 제목, 작성일 )
--- front/src/index.jsx
+++ front/src/index.jsx
@@ -5,11 +5,11 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<React.StrictMode>
-		<Router basename={process.env.PUBLIC_URL}>
-			<App />
-		</Router>
-	</React.StrictMode>
+	// <React.StrictMode>
+	<Router basename={process.env.PUBLIC_URL}>
+		<App />
+	</Router>
+	/* </React.StrictMode> */
 );
 
 // If you want to start measuring performance in your app, pass a function
