d76127fe97a1c490c86384152c0cdddfb6f4a4ca
ymj3539 <ymj3539@naver.com>
2022-05-13 04:02:23 +0900

    댓글 - get,post,delete

--- front/src/components/Comment.jsx
+++ front/src/components/Comment.jsx
@@ -1,77 +1,98 @@
-/* eslint-disable react/no-access-state-in-setstate */
-/* eslint-disable react/destructuring-assignment */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/Comment.scss';
 import SingleComment from './SingleComment';
 
-class Comment extends React.Component {
-	constructor(props) {
-		super(props);
+function Comment() {
+	const [commentList, setCommentList] = useState();
 
-		// fetch(`https://elice-server.herokuapp.com/board/1/comments`, {
-		// 	method: 'GET',
-		// })
-		// 	.then((response) => response.json())
-		// 	.then((result) => {
-		// 		console.log('결과: ', result);
-		// 	});
+	// 댓글 가져오기(GET)
+	useEffect(() => {
+		fetch(`https://elice-server.herokuapp.com/board/1/comments`, {
+			method: 'GET',
+		})
+			.then((res) => res.json())
+			.then((result) => {
+				console.log(result.data);
+				// commentList에 result.data를 넣어줌
+				setCommentList(result.data);
+			});
+	}, []);
 
-		this.state = {
-			// 댓글 불러오는 부분
-			comment_list: [
-				{
-					uuid: 1,
-					writer: '김철수',
-					date: '2022-05-09',
-					content: '안녕하세요',
-				},
-				{
-					uuid: 2,
-					writer: '김영희',
-					date: '2022-05-10',
-					content: '반가워요',
-				},
-			],
-		};
-		this.addTweet = this.addTweet.bind(this);
-	}
-
-	addTweet() {
+	// 댓글 추가
+	const onCreate = () => {
+		// 새댓글이 추가되는 것을 화면에서 보이는 부분
 		const { value } = document.querySelector('#comment_textarea');
-		this.setState({
-			comment_list: [
-				...this.state.comment_list,
-				{
-					uuid: this.state.comment_list.length + 1,
-					nickname: '홍길동',
-					date: new Date().toISOString().slice(0, 10),
-					content: value,
-				},
-			],
-		});
-	}
+		const comment = {
+			comment_idx: commentList.length + 1,
+			nickname: '홍길동',
+			date: new Date().toISOString().slice(0, 10),
+			content: value,
+			profile: 2,
+		};
+		// concat 함수로 comment객체를 commentList에 추가
+		setCommentList(commentList.concat(comment));
+		// 새댓글 post
+		fetch(`https://elice-server.herokuapp.com/board/1/comments`, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				comment_idx: commentList.length + 1,
+				nickname: '홍길동',
+				date: new Date(),
+				content: value,
+				id: 'id3@gmail.com',
+				post_idx: 1,
+				profile: 2,
+			}),
+		})
+			.then((res) => res.json())
+			.then((result) => {
+				console.log(result.data);
+			});
+	};
 
-	render() {
-		return (
-			<div className="comment_container">
-				<div className="comment">
-					<span>
-						<textarea id="comment_textarea" placeholder="댓글 달기..." />
-					</span>
-					<span>
-						<button type="submit" className="comment_btn" onClick={this.addTweet}>
-							등록
-						</button>
-					</span>
-				</div>
-				<ul className="comment_list">
-					{this.state.comment_list.map((tweet) => (
-						<SingleComment key={tweet.uuid} tweet={tweet} />
-					))}
-				</ul>
+	// 댓글 삭제
+	const onRemove = (param) => {
+		// param은 SingleComment.jsx에서 singleComment.comment_idx를 전달 받음
+
+		// 화면에서 보여지는 삭제
+		// comment_idx 가 전달받은 파라미터와 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+		// = comment.id 가 id 인 것을 제거함
+		setCommentList(commentList.filter((commentList) => commentList.comment_idx !== param));
+
+		// fetch delete
+		fetch(`https://elice-server.herokuapp.com/board/1/comments/${param}`, {
+			method: 'DELETE',
+			body: JSON.stringify({
+				comment_idx: param,
+			}),
+		})
+			.then((res) => res.json())
+			.then((result) => {
+				console.log(result);
+			});
+	};
+
+	return (
+		<div className="comment_container">
+			<div className="comment">
+				<span>
+					<textarea id="comment_textarea" placeholder="댓글 달기..." />
+				</span>
+				<span>
+					<button type="submit" className="comment_btn" onClick={onCreate}>
+						등록
+					</button>
+				</span>
 			</div>
-		);
-	}
+			<ul className="comment_list">
+				{commentList &&
+					commentList.map((singleComment) => (
+						<SingleComment key={singleComment.comment_idx} singleComment={singleComment} onRemove={onRemove} />
+					))}
+			</ul>
+		</div>
+	);
 }
 
 export default Comment;
--- front/src/components/SingleComment.jsx
+++ front/src/components/SingleComment.jsx
@@ -3,21 +3,24 @@
 import React from 'react';
 import profile from 'public/profile.jpeg';
 
-export default function SingleComment({ tweet }) {
+export default function SingleComment({ singleComment, onRemove }) {
 	return (
-		<div className="tweet">
+		<div className="singleComment">
 			<li className="comment_item">
 				<span className="comment_profile">
+					{/* 프로필 나중에 수정 */}
 					<img src={profile} alt="profile" />
 				</span>
 				<span>
-					<div className="comment_user_name">{tweet.nickname}</div>
-					<div className="comment_text">{tweet.content}</div>
-					<div className="comment_date">{tweet.date} </div>
+					<div className="comment_user_name">{singleComment.nickname}</div>
+					<div className="comment_text">{singleComment.content}</div>
+					<div className="comment_date">{singleComment.date.substr(0, 10)} </div>
 				</span>
 				<span className="comment_edit_button">
 					<button type="button">수정</button>
-					<button type="button">삭제</button>
+					<button type="submit" onClick={() => onRemove(singleComment.comment_idx)}>
+						삭제
+					</button>
 				</span>
 			</li>
 		</div>
