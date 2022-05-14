2cfae60427880275557d7535d8b216b5996498dc
Hye-Ji Na <nhj7911@gmail.com>
2022-05-14 22:35:37 +0900

    post 생성자 date 수정

--- back/models/Post.model.js
+++ back/models/Post.model.js
@@ -6,12 +6,8 @@ const Post = function (post) {
 	this.id = post.id;
 	this.title = post.title;
 	this.content = post.content;
-	// const now = new Date(); // 현재 시간
-	// const utcNow = now.getTime() + now.getTimezoneOffset() * 60 * 1000; // 현재 시간을 utc로 변환한 밀리세컨드값
-	// const koreaTimeDiff = 9 * 60 * 60 * 1000; // 한국 시간은 UTC보다 9시간 빠름(9시간의 밀리세컨드 표현)
-	// const koreaNow = new Date(utcNow + koreaTimeDiff); // utc로 변환된 값을 한국 시간으로 변환시키기 위해 9시간(밀리세컨드)를 더함
-	// this.date = koreaNow;
-	this.date = new Date();
+	// this.date = new Date();
+	this.date = post.date;
 	this.nickname = post.nickname;
 };
 
