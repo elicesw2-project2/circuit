709831ebb8911befb2aea3e1be27bce5aca0fb9a
ymj3539 <ymj3539@naver.com>
2022-05-16 02:28:15 +0900

    pw 재확인

--- front/src/pages/SignUp.jsx
+++ front/src/pages/SignUp.jsx
@@ -1,15 +1,16 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable react/jsx-props-no-spreading */
-import React from 'react';
-// import styled from 'styled-components';
+import React, { useRef } from 'react';
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import '../styles/SignUp.scss';
 
 function SignUp() {
+	const pw = useRef(); // 비밀번호 재확인 할 때 필요
 	const navigate = useNavigate();
 	const {
 		register,
+		watch,
 		handleSubmit,
 		formState: { errors },
 	} = useForm();
@@ -26,14 +27,16 @@ function SignUp() {
 				console.log('결과: ', result);
 				if (result.status === 201) {
 					alert(result.message);
+					console.log(result);
 					// 이동할 페이지 작성
-					navigate('/');
-				} else {
+					navigate('/auth/login');
+				} else if (result.status === 409) {
 					// 회원가입 실패 메세지
-					alert('다시 시도해주세요.');
+					alert(result.message);
 				}
 			});
 	};
+	pw.current = watch('pw'); // name pw인 element 관찰
 
 	return (
 		<div className="signUpBox">
@@ -57,12 +60,19 @@ function SignUp() {
 				</p>
 				{errors.pw && <p id="signUp_errors">{errors.pw.message}</p>}
 
-				{/* 프로필부분은 수정해야 됨 */}
 				<p>
-					<input {...register('profile', { required: '필수 정보입니다.' })} id="profile" placeholder="프로필" />
-					<label htmlFor="profile">프로필</label>
+					<input
+						{...register('pw_check', { required: true, validate: (value) => value === pw.current })}
+						id="pw_check"
+						placeholder="비밀번호 확인"
+					/>
+					<label htmlFor="pw_check">비밀번호</label>
 				</p>
-				{errors.profile && <p id="signUp_errors">{errors.profile.message}</p>}
+				{errors.pw_check && errors.pw_check.type === 'required' && <p id="signUp_errors">비밀번호를 적어주세요</p>}
+
+				{errors.pw_check && errors.pw_check.type === 'validate' && (
+					<p id="signUp_errors">비밀번호가 일치하지 않습니다</p>
+				)}
 
 				<button className="signUp-button" type="submit">
 					가입하기
