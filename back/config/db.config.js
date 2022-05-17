99c7db6f1929bf1824ee0fe2f749612e80a817fb
siwon <72402014+cone26@users.noreply.github.com>
2022-05-17 20:52:06 +0900

    env 또 수정

--- back/config/db.config.js
+++ back/config/db.config.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const dbConfig = {
 	HOST: process.env.DB_HOST,
-	PORT: parseInt(process.env.DB_PORT),
+	PORT: process.env.DB_PORT,
 	USER: process.env.DB_USER,
 	PASSWORD: process.env.DB_PASSWORD,
 	DB: process.env.DB_DB,
