cfcd9d53ecc134964b99f7bd8d87041d3ffbe3b6
siwon <72402014+cone26@users.noreply.github.com>
2022-05-17 20:20:26 +0900

    env 수정

--- back/config/db.config.js
+++ back/config/db.config.js
@@ -1,9 +1,13 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const dbConfig = {
-	HOST: 'dt3bgg3gu6nqye5f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-	PORT: 3306,
-	USER: 't0dn6h8s29ti3206',
-	PASSWORD: 'jnl72bdbmthtu24l',
-	DB: 'giw3qxk1yvxgfe3k',
+	HOST: process.env.DB_HOST,
+	PORT: process.env.DB_PORT,
+	USER: process.env.DB_USER,
+	PASSWORD: process.env.DB_PASSWORD,
+	DB: process.env.DB_DB,
 };
 
 export default dbConfig;
