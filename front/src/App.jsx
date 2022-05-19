1311b66cca08061bcb7177638d54abf22f23e11b
ctd <qpflapffhs76@gmail.com>
2022-05-19 15:17:30 +0900

    반응형 웹 만들기

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import './App.css';
+
 import { Routes, Route } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
+
 import PrivateRoute from 'utils/PrivateRoute';
 import PublicRoute from 'utils/PublicRoute';
 
@@ -23,100 +26,113 @@ function App() {
 	const [searchWritings, setSearchWritings] = useState();
 	const [email, setEmail] = useState('');
 
+	function Desktop({ children }) {
+		const isDesktop = useMediaQuery({ minWidth: 1224 });
+		return isDesktop ? children : null;
+	}
+
+	function Mobile({ children }) {
+		const isMobile = useMediaQuery({ maxWidth: 1223 });
+		return isMobile ? children : null;
+	}
+
 	if (window.location.pathname === '/circuit' || window.location.pathname === '/circuit/') {
 		window.location.pathname = '/circuit/page=1';
 	}
 
 	return (
 		<div className="App">
-			<Routes>
-				<Route element={<PublicRoute />}>
-					<Route path="/auth/login" element={<Login />} />
-					<Route path="/auth/signup" element={<SignUp />} />
-				</Route>
-				<Route element={<PrivateRoute />}>
-					<Route
-						element={
-							<NavBar
-								imgSrc={imgSrc}
-								setImgSrc={setImgSrc}
-								setNickname={setNickname}
-								setEmail={setEmail}
-								setDescription={setDescription}
-							/>
-						}
-					>
-						<Route
-							path="/"
-							element={
-								<MainPage
-									imgSrc={imgSrc}
-									setImgSrc={setImgSrc}
-									nickname={nickname}
-									setNickname={setNickname}
-									searchKeyword={searchKeyword}
-									setSearchKeyword={setSearchKeyword}
-									searchWritings={searchWritings}
-									setSearchWritings={setSearchWritings}
-									email={email}
-									setEmail={setEmail}
-								/>
-							}
-						/>
-						<Route
-							path="/page=:pageNum"
-							element={
-								<MainPage
-									imgSrc={imgSrc}
-									setImgSrc={setImgSrc}
-									nickname={nickname}
-									setNickname={setNickname}
-									searchWritings={searchWritings}
-									searchKeyword={searchKeyword}
-									setSearchKeyword={setSearchKeyword}
-									setSearchWritings={setSearchWritings}
-									email={email}
-									setEmail={setEmail}
-								/>
-							}
-						/>
+			<Desktop>
+				<Routes>
+					<Route element={<PublicRoute />}>
+						<Route path="/auth/login" element={<Login />} />
+						<Route path="/auth/signup" element={<SignUp />} />
 					</Route>
-					<Route
-						element={
-							<NavBar2
-								imgSrc={imgSrc}
-								setImgSrc={setImgSrc}
-								setNickname={setNickname}
-								setEmail={setEmail}
-								setDescription={setDescription}
-							/>
-						}
-					>
+					<Route element={<PrivateRoute />}>
 						<Route
-							path="/user/:id"
 							element={
-								<MyPage
+								<NavBar
 									imgSrc={imgSrc}
 									setImgSrc={setImgSrc}
-									nickname={nickname}
 									setNickname={setNickname}
 									setEmail={setEmail}
-									description={description}
 									setDescription={setDescription}
 								/>
 							}
-						/>
+						>
+							<Route
+								path="/"
+								element={
+									<MainPage
+										imgSrc={imgSrc}
+										setImgSrc={setImgSrc}
+										nickname={nickname}
+										setNickname={setNickname}
+										searchKeyword={searchKeyword}
+										setSearchKeyword={setSearchKeyword}
+										searchWritings={searchWritings}
+										setSearchWritings={setSearchWritings}
+										email={email}
+										setEmail={setEmail}
+									/>
+								}
+							/>
+							<Route
+								path="/page=:pageNum"
+								element={
+									<MainPage
+										imgSrc={imgSrc}
+										setImgSrc={setImgSrc}
+										nickname={nickname}
+										setNickname={setNickname}
+										searchWritings={searchWritings}
+										searchKeyword={searchKeyword}
+										setSearchKeyword={setSearchKeyword}
+										setSearchWritings={setSearchWritings}
+										email={email}
+										setEmail={setEmail}
+									/>
+								}
+							/>
+						</Route>
 						<Route
-							path="/page=:pageNum/Read=:id"
-							element={<Read nickname={nickname} imgSrc={imgSrc} email={email} />}
-						/>
-						<Route path="/Read=:id" element={<Read nickname={nickname} imgSrc={imgSrc} email={email} />} />
-						<Route path="/Writing" element={<Writing nickname={nickname} />} />
-						<Route path="/page=:pageNum/Writing=:id" element={<WritePut />} />
-						<Route path="/Writing=:id" element={<WritePut />} />
+							element={
+								<NavBar2
+									imgSrc={imgSrc}
+									setImgSrc={setImgSrc}
+									setNickname={setNickname}
+									setEmail={setEmail}
+									setDescription={setDescription}
+								/>
+							}
+						>
+							<Route
+								path="/user/:id"
+								element={
+									<MyPage
+										imgSrc={imgSrc}
+										setImgSrc={setImgSrc}
+										nickname={nickname}
+										setNickname={setNickname}
+										setEmail={setEmail}
+										description={description}
+										setDescription={setDescription}
+									/>
+								}
+							/>
+							<Route
+								path="/page=:pageNum/Read=:id"
+								element={<Read nickname={nickname} imgSrc={imgSrc} email={email} />}
+							/>
+							<Route path="/Read=:id" element={<Read nickname={nickname} imgSrc={imgSrc} email={email} />} />
+							<Route path="/Writing" element={<Writing nickname={nickname} />} />
+							<Route path="/page=:pageNum/Writing=:id" element={<WritePut />} />
+							<Route path="/Writing=:id" element={<WritePut />} />
+						</Route>
 					</Route>
-				</Route>
-			</Routes>
+				</Routes>
+			</Desktop>
+			<Mobile className="Mobile">여긴 너무 작은데요</Mobile>
 		</div>
 	);
 }
