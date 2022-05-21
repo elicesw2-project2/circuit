0e98e3b58a7b652ffff0759bf8591c6be9d80fe8
ctd <qpflapffhs76@gmail.com>
2022-05-21 23:10:27 +0900

    Google Analytics 추가

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -11,6 +11,7 @@ import MyPage from 'pages/MyPage';
 import profile from 'public/profile.jpg';
 import NavBar from 'components/NavBar';
 import NavBar2 from 'components/NavBar2';
+import useGAPageTracking from 'PageTracker';
 import MainPage from './pages/MainPage';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
@@ -37,6 +38,7 @@ function App() {
 		}
 	}, []);
 
+	useGAPageTracking();
 	return (
 		<div className="App">
 			{isPc && (
